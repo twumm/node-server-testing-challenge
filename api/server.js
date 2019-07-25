e2617954a6b452928801defa847c2aae07a95bc2
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-25 13:46:12 +0000

    feature: Add CRUD method to get all users

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Users = require('../api/users/usersModel');
 
 const server = express();
 
@@ -8,6 +9,22 @@ server.get('/', (req, res) => {
   res
     .status(200)
     .json({ content: 'up' });
+})
+
+server.get('/api/users', async (req, res) => {
+  Users.getAll()
+    .then(users => {
+      res
+        .status(200)
+        .json(users);
+    })
+    .catch(error => {
+      const err = {
+        message: error.message,
+        stack: error.stack,
+      };
+      res.status(500).json(err);
+    });
 })
 
 module.exports = server;
